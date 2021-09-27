300bc903590056b60c4dddbaa1e1f5a6d5538b82
DonAzi <savaSh-00@mail.ru>
2021-09-27 17:12:20 +0500

    anotherUpdate

--- m1_d6_hw.js
+++ m1_d6_hw.js
@@ -9,35 +9,58 @@ ASSIGNMENT RULES
 - Complete as many exercises as you can
 */
 
+const { doesNotMatch } = require("assert")
+
 // JS Basics
 
 /* EXERCISE A
    Create a variable called test and assign a string value to it.
 */
+let test = "newTest"
+// console.log(test)
 
 /* EXERCISE B
     Create a variable called sum and assign to it the result of the sum between the numbers 10 and 20.
 */
+let sum = 10+20
+// console.log(sum)
 
 /* EXERCISE C
     Create a variable called random and assign to it a random number between 0 and 20 (it should be randomly created at each execution).
 */
 
+let random = Math.floor(Math.random()* 20 +1 )
+console.log(random)
+
 /* EXERCISE D
     Create a variable called me and assign to it an object containing the following information: name = your name, surname = your surname, age = your age.
 */
+let me = {
+  name: "Don",
+  surname: "Az",
+  age: 23,
+  }
 
 /* EXERCISE E
     Write a piece of code for programmatically removing the age property from the previously create object.
 */
-
+  delete me.age
+      // console.log(me)
 /* EXERCISE F
    Write a piece of code for programmatically adding to the me object you defined before an array called skills, containing the programming languages you know right now.
 */
 
+me.skills = {
+   Java: "beginner",
+   HTM: "BEginner",
+   CSS:"Beginner",
+   JavaScript: "Beginner"
+}
+    // console.log(me)
 /* EXERCISE G
    Write a piece of code for programmatically removing the last skill from the skills array inside the me object.
 */
+
 
 // JS Functions
 /* EXERCISE 1
